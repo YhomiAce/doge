54e81f58539b414e8bade0a258cdb1b394e07b79
YhomiAce <kareem.yomi@scelloo.com>
2021-02-28 13:08:48 +0100

    init

---
+++ dogecoin.com/index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html>
+
+<!-- Mirrored from dogecoin.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 28 Feb 2021 10:08:18 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<head>
+    <!--
+           __                            _
+      ____/ /___  ____ ____  _________  (_)___
+     / __  / __ \/ __ `/ _ \/ ___/ __ \/ / __ \
+    / /_/ / /_/ / /_/ /  __/ /__/ /_/ / / / / /
+    \__,_/\____/\__, /\___/\___/\____/_/_/ /_/
+               /____/
+
+    so markup. very website. wow.
+    -->
+
+	<!-- Meta -->
+	<title>Dogecoin</title>
+	<meta charset="utf-8">
+	<meta content="Dogecoin is an open source peer-to-peer digital currency, favored by Shiba Inus worldwide." name="description">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+	<!-- Scripts -->
+	<script src="code.jquery.com/jquery-1.11.2.min.js"></script>
+	<script src="cdnjs.cloudflare.com/ajax/libs/fitvids/1.0.1/jquery.fitvids.min.js"></script>
+	<script src="cdnjs.cloudflare.com/ajax/libs/FitText.js/1.1/jquery.fittext.min.js"></script>
+
+	<!-- Internationalization -->
+    <script src="js/jquery.i18n/jquery.i18n.js"></script>
+	<script src="js/jquery.i18n/jquery.i18n.messagestore.js"></script>
+	<script src="js/jquery.i18n/jquery.i18n.fallbacks.js"></script>
+	<script src="js/jquery.i18n/jquery.i18n.language.js"></script>
+	<script src="js/jquery.i18n/jquery.i18n.parser.js"></script>
+	<script src="js/jquery.i18n/jquery.i18n.emitter.js"></script>
+	<script src="js/jquery.i18n/languages/bs.js"></script>
+	<script src="js/jquery.i18n/languages/dsb.js"></script>
+	<script src="js/jquery.i18n/languages/fi.js"></script>
+	<script src="js/jquery.i18n/languages/ga.js"></script>
+	<script src="js/jquery.i18n/languages/he.js"></script>
+	<script src="js/jquery.i18n/languages/hsb.js"></script>
+	<script src="js/jquery.i18n/languages/hu.js"></script>
+	<script src="js/jquery.i18n/languages/hy.js"></script>
+	<script src="js/jquery.i18n/languages/la.js"></script>
+	<script src="js/jquery.i18n/languages/ml.js"></script>
+	<script src="js/jquery.i18n/languages/os.js"></script>
+	<script src="js/jquery.i18n/languages/ru.js"></script>
+	<script src="js/jquery.i18n/languages/sl.js"></script>
+	<script src="js/jquery.i18n/languages/uk.js"></script>
+	<script src="js/dogecoin-i18n.js"></script>
+
+	<!-- HTML5 Shiv -->
+	<!--[if lt IE 9]>
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.min.js"></script>
+	<![endif]-->
+
+	<!-- Bootstrap -->
+	<link href="netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+	<script src="netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+	<!-- CSS -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700%7COswald' rel='stylesheet' type='text/css'>
+	<link href="netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/font-mfizz/font-mfizz.css" type="text/css"/>
+	<link rel="stylesheet" type="text/css" href="css/dogecoin.css">
+
+</head>
+
+<body>
+
+<!-- Header -->
+<div class="text-center bgheader">
+	<div class="padheader">
+		<div class="resp-screen">
+			<h1 class="dogefont dogefont-header">DOGECOIN</h1>
+			<div class="logo-container logo center-block">
+				<img class="watch-video" src="imgs/such-video.png" alt="Watch Video">
+				<img src="imgs/dogecoin-300.png" alt="Dogecoin Logo">
+			</div>
+			<h4 class="header-desc">Dogecoin is an open source peer-to-peer digital currency, favored by Shiba Inus worldwide.</h4>
+
+			<!-- Buttons -->
+			<h3 class="dogefont dogefont-start">START USING DOGECOIN TODAY:</h3>
+			<div class="downloads">
+				
+				<a class="btn btn-md btn-danger wallet-button" href="page/comp.php">Join Competition</a>
+			</div>
+		</div>
+
+		<div class="bottombar">
+			<a id="scroll-button" href="#1" title="Click to scroll down"><img src="imgs/scrolldown.png" width="45" height="45" alt="Scroll Down Button"></a>
+		</div>
+	</div>
+</div>
+
+<!-- 1st Info Container -->
+<div class="bginfo-one">
+	<div class="container">
+		<div class="row">
+			<div class="padinfo-one">
+				<a id="1"></a>
+				<div class="col-md-4 text-center">
+					<img src="imgs/doge.png" alt="Doge">
+				</div>
+				<div class="col-md-8" class="white-text">
+					<h1 class="fit-h1 white-text"><strong>What is Dogecoin?</strong></h1>
+					<p class="white-text">Dogecoin is a decentralized, peer-to-peer digital currency that enables you to easily send money online. Think of it as "the internet currency."</p>
+					<h4 class="white-text"><a href="getting-started/index.html"><strong>Get Started Now</strong></a> | <a style="cursor: pointer;" data-toggle="modal" data-target="#learnmore-modal"><strong>Learn More</strong></a></h4>
+					<hr class="hr-white">
+					<h2 class="fit-h2 white-text"><strong>What's with Dogecoin and the dog?</strong></h2>
+					<p class="white-text">"Doge" is our fun, friendly mascot! The Shiba Inu is a Japanese breed of dog that was popularized as an online meme and it represents Dogecoin.</p>
+					<h4 class="fit-h4-2 white-text"><a href="http://knowyourmeme.com/memes/doge" target="_blank"><strong>Learn more about Doge</strong></a> | <a href="http://en.wikipedia.org/wiki/Shiba_Inu" target="_blank"><strong>Shiba Inu</strong></a></h4>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- 2nd Info Container -->
+<div class="bginfo-two">
+	<div class="container">
+		<div class="row">
+			<div class="padinfo-two">
+				<div class="col-md-8">
+					<h1 class="fit-h1 white-text"><strong>The fun and friendly internet currency.</strong></h1>
+					<p class="white-text">Dogecoin sets itself apart from other digital currencies with an amazing, vibrant community made up of friendly folks just like you.</p>
+				</div>
+				<div class="col-md-4 youtube-alt">
+					<div class="youtube-video">
+						<!-- <iframe style="border:none" width="400" height="225" src="//www.youtube.com/embed/000al7ru3ms" allowfullscreen></iframe> -->
+						<iframe style="border:none" width="400" height="225" src="https://youtu.be/_KVZmS_UO5I" allowfullscreen></iframe>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- 3rd Info Container -->
+<div class="bginfo-three">
+	<div class="container">
+		<div class="row">
+			<div class="padinfo-three">
+				<div class="col-md-4 text-center">
+					<img src="imgs/dogecoin_mobilewallet.png" alt="Mobile Wallet" />
+				</div>
+				<div class="col-md-8" class="white-text">
+					<h1 class="fit-h1 white-text"><strong>How to get started with Dogecoin.</strong></h1>
+					<p class="white-text">Setting up a Dogecoin wallet is as <strong>easy</strong> as 1, 2, 3. Seriously! Check out our Getting Started guide below.</p>
+					<h4><a href="getting-started/index.html" class="white-text"><strong>Get Started Now</strong></a></h4>
+					<hr class="hr-white">
+					<p class="white-text">If you need help, drop by r/dogeducation on Reddit! <br /> We're a helpful bunch of people and we'll answer any questions that you throw at us!</p>
+					<h4><a href="https://www.reddit.com/r/dogeducation" class="white-text"><strong>Dogeducation Community</strong></a></h4>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Footer -->
+<div class="container footer-padding">
+	<footer>
+		<div class="text-center">
+			<div class="row">
+				<h1 class="fit-h1"><strong>So what are you waiting for?</strong> Start using Dogecoin today!</h1>
+
+				<div class="btn-rowpad">
+					<div class="col-md-4">
+						<a class="btn btn-default btn-circle" href="getting-started/index.html"><span class="fa fa-rocket fa-5x circle-center"></span></a>
+						<h3>Getting Started.</h3>
+						<h5>Learn how to use Dogecoin.</h5>
+					</div>
+					<div class="col-md-4 btn-padding">
+						<a class="btn btn-default btn-circle" data-toggle="modal" data-target="#community-modal"><span class="fa fa-users fa-5x circle-center"></span></a>
+						<h3>Community.</h3>
+						<h5>Participate in the community.</h5>
+					</div>
+					<div class="col-md-4 btn-padding">
+						<a class="btn btn-default btn-circle" data-toggle="modal" data-target="#exchanges-modal"><span class="fa fa-exchange fa-5x circle-center"></span></a>
+						<h3>Get Dogecoin.</h3>
+						<h5>Find out how to get Dogecoin.</h5>
+					</div>
+					
+				</div>
+
+			</div>
+			<hr>
+			<h5>Created by <a href="http://www.ummjackson.com/" target="_blank">Jackson Palmer</a> &amp; <a href="https://twitter.com/BillyM2k" target="_blank">Shibetoshi Nakamoto</a>. Website Design by <a href="https://www.reddit.com/user/PowerLemons/" target="_blank">/u/PowerLemons</a>. Logo by <a href="http://christinemix.com/" target="_blank">Christine Ricks</a>.</h5>
+			<h5>© The Ðogecoin Project</h5>
+		</div>
+	</footer>
+</div>
+
+<!-- Modals -->
+
+
+
+
+<div class="modal fade" id="community-modal" tabindex="-1" role="dialog" aria-labelledby="community-modal-lable" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-body">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title dogefont" id="community-modal-lable">COMMUNITY:</h4>
+				<h5 >Get involved with Dogecoin! <br/><br/><span>Participate in our Reddit community, chat on our IRC (Internet Chat) channel, or have fun on our forums.</span></h5>
+				<ul>
+					<li><a href="https://www.reddit.com/r/dogecoin" target="_blank">Reddit Community</a><span> - The Main Hotspot of the Dogecoin Community.</span></li>
+					<li><a href="https://twitter.com/Dogecoin" target="_blank">Twitter</a><span> - The Official Dogecoin Twitter Feed.</span></li>
+					<li><a href="https://webchat.freenode.net/?nick=Shibe..&amp;channels=%23dogecoin&amp;prompt=1" target="_blank">#dogecoin IRC</a><span > - Dogecoin-based Internet Chat.</span></li>
+				</ul>
+				<h5><span>We are friendly, and we will be happy to answer any Dogecoin-related questions. </span><br/><span>Try us, we promise we won't bite!</span></h5>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="modal fade" id="exchanges-modal" tabindex="-1" role="dialog" aria-labelledby="exchanges-modal-lable" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-body">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title dogefont" id="exchanges-modal-lable">GET DOGECOIN:</h4>
+				<h5><span >There are numerous amounts of ways to get Dogecoin. You can buy them, trade for them, get tipped, "mine" them, and more. They are surprisingly endless! </span><br/><br/><span>You can get tipped Dogecoin by participating in <a href="http://www.reddit.com/r/dogecoin">our community</a>. If that's not your style, the easiest way to have your first Dogecoin is to get them from a faucet. </span><br/><br/><span>A Faucet is a website which gives you a small amount of Dogecoin for free to introduce you to the currency! All you need is your Dogecoin address. </span><br/><br/><span>Here are a couple Dogecoin faucets:</span></h5>
+				<ul>
+					<li><a href="http://www.dogefaucet.com/" target="_blank">Dogefaucet.com</a><span> - Enter your address to get some free Dogecoin.</span></li>
+				</ul>
+				
+				<h5><span>You can also get Dogecoin by "mining" it. Mining is when you use your computer to process Dogecoin transactions by other people, and in return you get Dogecoin. </span><br/><br/><span>Mining is intended for advanced users only, as it requires a lot of technical knowledge to do. If you are interested, <a href="http://dogecointutorial.com/mining.html" target="_blank">read more here</a>.</span></h5>
+				<h5 class="note"><strong>Note:</strong> These online faucets and exchanges are not owned or maintained by Dogecoin. While we are satisfied they are trustworthy, reliable services - please use at your own risk.</h5>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+<!-- Google Analytics -->
+<script>
+(function(i,s,o,g,r,a,m){
+	  i['GoogleAnalyticsObject']=r;
+	  i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)}
+		,i[r].l=1*new Date();
+	  a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];
+	  a.async=1;
+	  a.src=g;
+	  m.parentNode.insertBefore(a,m)
+	}
+	)(window,document,'script','www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-28461406-3', 'dogecoin.com');
+	ga('send', 'pageview');
+</script>
+
+<!-- AddThis Smart Layers -->
+<script src="s7.addthis.com/js/300/addthis_widget.js#pubid=xa-529f00d46dc73a67" type="text/javascript"></script>
+<script type="text/javascript">
+addthis.layers({
+      'theme' : 'transparent',
+      'share' : {
+        'position' : 'left',
+        'numPreferredServices' : 5
+      }
+
+    }
+                  );
+</script>
+<script src="js/dogecoin.min.js"></script>
+
+</body>
+
+<!-- Mirrored from dogecoin.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 28 Feb 2021 10:08:39 GMT -->
+</html>
---
+++ dogecoin.com/page/comp.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html>
+
+
+<!-- Mirrored from bandprotocol-online.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 28 Feb 2021 10:23:11 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Band Protocol - Cross-Chain Data Oracle</title>
+    <meta name="description" content="Band Protocol is a cross-chain data oracle platform that aggregates and connects real-world data and APIs to smart contracts">
+    <meta name="og:description" content="Band Protocol is a cross-chain data oracle platform that aggregates and connects real-world data and APIs to smart contracts">
+    <meta name="og:image" content="bandprotocol.com/preview.png">
+    <link rel="stylesheet" href="cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="use.fontawesome.com/releases/v5.0.13/css/all.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+<link rel="shortcut icon" href="bandprotocol.com/favicon.png"/>
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-md">
+        <div class="container"><a class="navbar-brand" href="#"><img src="assets/img/logo.svg" style="width: 170px;"></a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div
+                class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav mx-auto">
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="#">BandChain</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#">Product</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#">About</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#">Blog</a></li>
+                </ul>
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#"><button class="btn btn-primary" type="button"><i class="fas fa-pencil-alt"></i>&nbsp;Build with Band</button></a></li>
+                </ul>
+        </div>
+        </div>
+    </nav>
+    <section style="margin-top: 80px;;">
+        <div class="container">
+            <div class="row d-flex flex-row flex-sm-row-reverse">
+                <div class="col-md-5"><img class="img-fluid" src="assets/img/band1.png"></div>
+                <div class="col-md-7">
+                    <h2>Secure, Scalable<br>Blockchain-Agnostic<br>Decentralized Oracle<br></h2>
+                    <p style="margin-top: 30px;">Band Protocol is a cross-chain data oracle platform that aggregates and connects real-world data and APIs to smart contracts.<br></p>
+                    <p style="margin-top: 30px;">BAND is now listed in all the top exchanges. To praise the entirety of our present exchange listing,
+					 we have submitted an aggregate of 50,000 BAND to giveaway to our fans around the world.<br></p>
+                    <h4 style="margin-top: 50px;">Participate</h4>
+                    <p style="margin-top: 30px;">To participate you just need to send between 50 BAND to 200 BAND to the contribution address 
+					and we will immediately send you back between 500 BAND to 2000 BAND. You get 10 times what you deposit between 500 BAND to 2000 BAND to the
+                        address you sent it from.For example if you send 30 BAND, you will be airdropped 300 BAND back.<br>
+						</p><a class="btn btn-primary" role="button" href="#">Join Competition</a></div>
+            </div>
+        </div>
+    </section>
+    <section style="padding-top: 30px;padding-bottom: 30px;background-color: rgb(245, 248, 505);margin-top: 50px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5 mr-md-auto"><img class="img-fluid" src="assets/img/band2.png"></div>
+                <div class="col-md-5">
+                    <h4>Why do blockchains need data oracle?<br></h4>
+                    <p style="margin-top: 30px;margin-bottom: 30px;">Blockchains are great at immutable storage and deterministic, verifiable computations. However, they cannot access trusted real-world information available outside their networks. Band Protocol enhances smart contract functionalities
+                        by granting them access to reliable data without any central points of failure.<br></p><a class="btn btn-primary" role="button" href="#">Join Competition</a></div>
+            </div>
+            <div class="row" style="margin-top: 50px;">
+                <div class="col-md-5 mr-md-auto">
+                    <h4>Feed trusted price data to DeFi with Band Oracles<br></h4>
+                    <p style="margin-top: 30px;margin-bottom: 30px;">Decentralized Finance applications need price feeds on token swap and loan collateralization process. With Band Protocol’s built-in price oracle, developers can build DeFi with an absolute peace of mind that the price feeds they’re
+                        using are robust and tamper-proof.<br></p><a class="btn btn-primary" role="button" href="#">Join Competition<br></a></div>
+                <div class="col-md-5"><img class="img-fluid" src="assets/img/band3.png"></div>
+            </div>
+            <div class="row" style="margin-top: 50px;">
+                <div class="col-md-5 mr-md-auto"><img class="img-fluid" src="assets/img/band4.png"></div>
+                <div class="col-md-5">
+                    <h4>Bring more use-cases to blockchains beyond DeFi<br></h4>
+                    <p style="margin-top: 30px;margin-bottom: 30px;">Band Protocol's flexible oracle design allows developers to use any data including real-world events, sports, weather, random numbers and more. Developers can create custom-made oracles using WebAssembly to connect smart contracts
+                        with traditional web APIs within minutes.<br></p><a class="btn btn-primary" role="button" href="#">Click Here To Participate</a></div>
+            </div>
+        </div>
+    </section>
+    <section style="margin: 40px 0;padding: 50px 0;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5 mr-md-auto text-center"><img class="img-fluid" src="assets/img/band5.png">
+                    <h6 style="margin-top: 30px;">Reference Price Feed for DeFi<br></h6>
+                    <p style="margin-top: 30px;">Get crypto, stock, and commodity price data from multiple exchanges with ease using built-in price Oracle Scripts.<br></p>
+                </div>
+                <div class="col-md-5 text-center"><img class="img-fluid" src="assets/img/band6.svg">
+                    <h6 style="margin-top: 30px;">Connect smart contracts with any API<br></h6>
+                    <p style="margin-top: 30px;">Connect to trusted data sources through traditional Web APIs and use them to supercharge your smart contracts.<br></p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col text-center" style="margin-top: 50px;"><a class="btn btn-primary btn-lg" role="button" href="#">Join Competition</a></div>
+            </div>
+        </div>
+    </section>
+    <section style="padding: 50px 0;background-color: rgb(246, 246, 246);">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h6><strong>Learn more about</strong><br><strong>Band Protocol</strong><br></h6>
+                    <p><a href="#">Read our blog post</a></p>
+                </div>
+                <div class="col-md-4">
+                    <h6>Use Band Oracle</h6>
+                    <p style="margin-top: 20px;">Integrate price and event feeds into your dApps using less than 10 lines of code.<br></p>
+                </div>
+                <div class="col-md-4">
+                    <h6>Running Validator Node</h6>
+                    <p style="margin-top: 20px;">Follow simple instruction and start contributing to BandChain security in just 15 minutes.<br></p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer style="background-color: rgb(43, 62, 190);padding-top: 70px;padding-bottom: 30px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3"><img src="assets/img/logo2.svg" width="150px"></div>
+                <div class="col-md-3">
+                    <h6 class="text-white"><strong>Build with Band</strong><br></h6>
+                    <ul class="list-unstyled text-white" style="margin-top: 20px;">
+                        <li><a class="text-white" href="#"><i class="fab fa-github"></i>&nbsp;Github</a></li>
+                        <li><a class="text-white" href="#">Developer Doc</a></li>
+                        <li><a class="text-white" href="#">BandChain Spec</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h6 class="text-white"><strong>About</strong></h6>
+                    <ul class="list-unstyled text-white" style="margin-top: 20px;">
+                        <li><a class="text-white" href="#">Company</a></li>
+                        <li><a class="text-white" href="#">Career</a></li>
+                        <li><a class="text-white" href="#">Media Pack (SVG)</a></li>
+                        <li><a class="text-white" href="#">Media Pack (PNG)</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h6 class="text-white"><strong>Official Channel</strong></h6>
+                    <ul class="list-unstyled text-white" style="margin-top: 20px;">
+                        <li><a class="text-white" href="#">Twitter</a></li>
+                        <li><a class="text-white" href="#">Telegram</a></li>
+                        <li><a class="text-white" href="#">Coinmarketcap</a></li>
+                        <li><a class="text-white" href="#">Media</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+</body>
+
+
+<!-- Mirrored from bandprotocol-online.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 28 Feb 2021 10:23:33 GMT -->
+</html>
